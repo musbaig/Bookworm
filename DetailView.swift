8262491ab318f2a305802ade1c637127c4ed60d6
Mus B <muslimbaig@icloud.com>
2022-12-13 01:46:59 -0500

    feat: detail view initial pass

---
+++ DetailView.swift
@@ -0,0 +1,34 @@
+//
+//  DetailView.swift
+//  Bookworm
+//
+//  Created by Adela Bonoiu on 12/13/22.
+//
+
+import CoreData
+import SwiftUI
+
+struct DetailView: View {
+    let book: Book
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static let moc = NSManagedObjectContext(concurrencyType: .mainQueueConcurrencyType)
+
+        static var previews: some View {
+            let book = Book(context: moc)
+            book.title = "Test book"
+            book.author = "Test author"
+            book.genre = "Fantasy"
+            book.rating = 4
+            book.review = "This was a great book; I really enjoyed it."
+
+            return NavigationView {
+                DetailView(book: book)
+            }
+        }
+}
